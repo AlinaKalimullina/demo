4716390166537b78caebb6388b9217c659a4ce90
Kalimullina Alina <kalimullina.lina2018@yandex.ru>
2023-02-08 14:43:49 +0300

    рефактор

--- tests/front/SavePhoneNumber.js
+++ tests/front/SavePhoneNumber.js
@@ -10,10 +10,10 @@ describe('restore pass', function() {
         await browser
             .verify.visible(loginInput)
             .setValue(loginInput, process.env.ADMIN_PHONE)
-            .assert.attributeContains(loginInput, 'data-mask-raw-value', process.env.ADMIN_PHONE)
+            .assert.attributeContains('input[type=text]', 'data-mask-raw-value', process.env.ADMIN_PHONE)
             .click(dontRemberPass)
             .assert.urlContains('/auth/forgot')
-            .assert.attributeContains(restoreInput, 'data-mask-raw-value', process.env.ADMIN_PHONE)
+            .assert.attributeContains('input.el-input__inner', 'data-mask-raw-value', process.env.ADMIN_PHONE)
     });
 
     after(browser => browser.end());
