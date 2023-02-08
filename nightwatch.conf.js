a492abd5264541cf0f8283e83e527c08f499ec5a
Kalimullina Alina <kalimullina.lina2018@yandex.ru>
2023-02-08 10:36:34 +0300

    fix

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -65,7 +65,7 @@ module.exports = {
             //'--no-sandbox',
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
-            '--headless'
+            //'--headless'
           ]
         }
       },
