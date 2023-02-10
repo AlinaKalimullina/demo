94dc27a8eecfdcd3411b7e62dd79b378d526b914
Kalimullina Alina <kalimullina.lina2018@yandex.ru>
2023-02-10 16:46:28 +0300

    прост проверка на visible

--- tests/front/incorrect.js
+++ tests/front/incorrect.js
@@ -5,7 +5,6 @@ describe('check validation', function() {
     const loginInput = element('input[type=text]')
     const passInput = element('input[type=password]')
     const enterButtonEl = element('button.el-button.el-button--primary')
-    const formEl = element('#app > div.auth-layout > div > form')
 
 
     it('enter incorrect data', async function() {
@@ -13,7 +12,7 @@ describe('check validation', function() {
             .setValue(loginInput, process.env.ADMIN_PHONE_INCORRECT)
             .setValue(passInput, process.env.ADMIN_PASS_INCORRECT)
             .click(enterButtonEl)
-            .assert.textContains(formEl, 'Неправильно введён номер телефона или пароль')
+            .assert.visible('#app > div.auth-layout > div > form > div.custom-input.has-placeholder.has-errors.keep-placeholder-on-focus.focused > div.error')
     })
 
     after(browser => browser.end());
