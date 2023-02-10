0b29845fc459fd20865890f6c3156bf1f610f1a4
Kalimullina Alina <kalimullina.lina2018@yandex.ru>
2023-02-10 16:31:59 +0300

    переопределила элемент

--- tests/front/incorrect.js
+++ tests/front/incorrect.js
@@ -5,7 +5,7 @@ describe('check validation', function() {
     const loginInput = element('input[type=text]')
     const passInput = element('input[type=password]')
     const enterButtonEl = element('button.el-button.el-button--primary')
-    const formEl = element('#app > div.auth-layout > div > form')
+    const formEl = element('#app > div.auth-layout > div > form > div.custom-input.has-placeholder.has-errors.keep-placeholder-on-focus.focused > div.error')
 
 
     it('enter incorrect data', async function() {
@@ -15,7 +15,7 @@ describe('check validation', function() {
             .assert.visible(passInput)
             .setValue(passInput, process.env.ADMIN_PASS_INCORRECT)
             .click(enterButtonEl)
-            .assert.hasClass(formEl, 'div.error')
+            .assert.hasClass(formEl, 'error')
     })
 
     after(browser => browser.end());
